12a42b4340a136b3721cafd06c2f66d6970c5b91
DenisLysko <denislysko@yandex.ru>
2023-02-23 18:00:48 +0300

    feat: Initializing WordDictionary

---
+++ EnglishWordStudy/EnglishWordStudy/WordDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace EnglishWordStudy
+{
+    internal class WordDictionary
+    {
+        public Dictionary<string, string> dic = new Dictionary<string, string>();
+
+        public double completionPercent = 0;
+
+        public void FillDictionary(string[] wordsAndTheirTranslations)
+        {
+            for (int i = 0; i < wordsAndTheirTranslations.Length; i = i + 2)
+            {
+                dic[wordsAndTheirTranslations[i]] = wordsAndTheirTranslations[i + 1];
+            }
+           
+        }
+    }
+}
