73db729ede314d9b6d90b4fa7c84009aafd1347d
DenisLysko <denislysko@yandex.ru>
2023-02-23 18:00:17 +0300

    feat: Initialaizing class for words

---
+++ EnglishWordStudy/EnglishWordStudy/Word.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishWordStudy
+{
+    internal class Word
+    {
+        public string? englishName;
+        public string? russianName;
+
+        public int? correctAnswersCounter;
+
+
+
+    }
+
+    
+}
